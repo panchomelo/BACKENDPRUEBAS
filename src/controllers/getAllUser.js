b73c0347b4a7239f9a471026e92a7d778c5904d7
franciscovega <fran.vgasalv@gmail.com>
2024-03-04 04:39:12 -0300

    olaa

--- src/controllers/getAllUser.js
+++ src/controllers/getAllUser.js
@@ -1,5 +1,14 @@
-const listarUsuarios = (req, res) => {
-    res.send("ruta GET ok");
-    }; 
+const User = require("../models/user.model.js")
 
+const listarUsuarios = async (req, res) => {
+
+    const usuarios = await User.find({});
+
+    res.status(200).json({
+        code: 200,
+        msg: "lista de usuarios",
+        data: usuarios
+    })
+}
+    
 module.exports = listarUsuarios;
