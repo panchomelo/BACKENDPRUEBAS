0a2442767d1a51090921d0c7b697d93aaeedd0da
franciscovega <fran.vgasalv@gmail.com>
2024-03-19 02:42:42 -0300

    a

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async () => { //funcion asincrona
-    try { //intenta
+    try { //intentaa
         await mongoose.connect('mongodb://localhost:27017/proyectoIntegrador'); //conectarse a base de datos local
         console.log("base de datos conectada");
     } catch (error) { //si hay un error capturalo, de esta manera la app sigue funcionando
